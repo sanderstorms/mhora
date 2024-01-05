d07733519d89d7d86ff76d7e4a0aabb6cf03c330
Sander Storms <sander.storms.urmond@gmail.com>
2024-01-05 18:04:31 +0100

    Started making string tables

---
+++ Tables/Varga.cs
@@ -0,0 +1,452 @@
+﻿namespace Mhora.Tables
+{
+	public static class Varga
+	{
+		public static readonly string[] mRulersHora =
+		{
+			"Devaas - Sun",
+			"Pitris - Moon"
+		};
+
+		public static readonly string[] mRulersDrekkana =
+		{
+			"Naarada",
+			"Agastya",
+			"Durvaasa"
+		};
+
+		public static readonly string[] mRulersChaturthamsa =
+		{
+			"Sanaka",
+			"Sanandana",
+			"Sanat Kumaara",
+			"Sanaatana"
+		};
+
+		public static readonly string[] mRulersSaptamsa =
+		{
+			"Kshaara",
+			"Ksheera",
+			"Dadhi",
+			"Ghrita",
+			"Ikshurasa",
+			"Madya",
+			"Shuddha Jala"
+		};
+
+		public static readonly string[] mRulersNavamsa =
+		{
+			"Deva",
+			"Nara",
+			"Rakshasa"
+		};
+
+		public static readonly string[] mRulersDasamsa =
+		{
+			"Indra",
+			"Agni",
+			"Yama",
+			"Rakshasa",
+			"Varuna",
+			"Vayu",
+			"Kubera",
+			"Ishana",
+			"Brahma",
+			"Ananta"
+		};
+
+		public static readonly string[] mRulersDwadasamsa =
+		{
+			"Ganesha",
+			"Ashwini Kumars",
+			"Yama",
+			"Sarpa"
+		};
+
+		public static readonly string[] mRulersShodasamsa =
+		{
+			"Brahma",
+			"Vishnu",
+			"Shiva",
+			"Surya"
+		};
+
+		public static readonly string[] mRulersVimsamsa =
+		{
+			"Kali",
+			"Gauri",
+			"Jaya",
+			"Lakshmi",
+			"Vijaya",
+			"Vimala",
+			"Sati",
+			"Tara",
+			"Jwalamukhi",
+			"Shaveta",
+			"Lalita",
+			"Bagla",
+			"Pratyangira",
+			"Shachi",
+			"Raudri",
+			"Bhavani",
+			"Varda",
+			"Jaya",
+			"Tripura",
+			"Sumukhi",
+			"Daya",
+			"Medha",
+			"China Shirsha",
+			"Pishachini",
+			"Dhoomavati",
+			"Matangi",
+			"Bala",
+			"Bhadra",
+			"Aruna",
+			"Anala",
+			"Pingala",
+			"Chuccuka",
+			"Ghora",
+			"Varahi",
+			"Vaishnavi",
+			"Sita",
+			"Bhuvaneshi",
+			"Bhairavi",
+			"Mangla",
+			"Aparajita"
+		};
+
+		public static readonly string[] mRulersChaturvimsamsa =
+		{
+			"Skanda",
+			"Parashudhara",
+			"Anala",
+			"Vishvakarma",
+			"Bhaga",
+			"Mitra",
+			"Maya",
+			"Antaka",
+			"Vrishdhwaja",
+			"Govinda",
+			"Madana",
+			"Bhima"
+		};
+
+		public static readonly string[] mRulersNakshatramsa =
+		{
+			"Ashwini Kumara",
+			"Yama",
+			"Agni",
+			"Brahma",
+			"Chandra Isa",
+			"Aditi",
+			"Jiva",
+			"Abhi",
+			"Pitara",
+			"Bhaga",
+			"Aryama",
+			"Surya",
+			"Tvashta",
+			"Maruta",
+			"Shakragni",
+			"Mitra",
+			"Indra",
+			"Rakshasa",
+			"Varuna",
+			"Vishvadeva",
+			"Brahma",
+			"Govinda",
+			"Vasu",
+			"Varuna",
+			"Ajapata",
+			"Ahirbudhnya",
+			"Pusha"
+		};
+
+		public static readonly string[] mRulersTrimsamsa =
+		{
+			"Agni",
+			"Vayu",
+			"Indra",
+			"Kubera",
+			"Varuna"
+		};
+
+		public static readonly string[] mRulersKhavedamsa =
+		{
+			"Vishnu",
+			"Chandra",
+			"Marichi",
+			"Twashta",
+			"Brahma",
+			"Shiva",
+			"Surya",
+			"Yama",
+			"Yakshesha",
+			"Ghandharva",
+			"Kala",
+			"Varuna"
+		};
+
+		public static readonly string[] mRulersAkshavedamsa =
+		{
+			"Brahma",
+			"Shiva",
+			"Vishnu"
+		};
+
+		public static readonly string[] mRulersShashtyamsa =
+		{
+			"Ghora",
+			"Rakshasa",
+			"Deva",
+			"Kubera",
+			"Yaksha",
+			"Kinnara",
+			"Bharashta",
+			"Kulaghna",
+			"Garala",
+			"Vahni",
+			"Maya",
+			"Purishaka",
+			"Apampathi",
+			"Marut",
+			"Kaala",
+			"Sarpa",
+			"Amrita",
+			"Indu",
+			"Mridu",
+			"Komala",
+			"Heramba",
+			"Brahma",
+			"Vishnu",
+			"Maheshwara",
+			"Deva",
+			"Ardra",
+			"Kalinasa",
+			"Kshitishwara",
+			"Kamalakara",
+			"Gulika",
+			"Mrityu",
+			"Kala",
+			"Davagni",
+			"Ghora",
+			"Yama",
+			"Kantaka",
+			"Sudha",
+			"Amrita",
+			"Poornachandra",
+			"Vishadagdha",
+			"Kulanasa",
+			"Vamsa Khaya",
+			"Utpata",
+			"Kala",
+			"Saumya",
+			"Komala",
+			"Sheetala",
+			"Karala damshtra",
+			"Chandramukhi",
+			"Praveena",
+			"Kala Pavaka",
+			"Dandayudha",
+			"Nirmala",
+			"Saumya",
+			"Kroora",
+			"AtiSheetala",
+			"Amrita",
+			"Payodhi",
+			"Bhramana",
+			"Chandrarekha"
+		};
+
+		public static string[] mRulersNadiamsaRajan =
+		{
+			"Vasudha",
+			"Vaishnavi",
+			"Brahmi",
+			"Kalakoota",
+			"Sankari",
+			"Sadaakari",
+			"Samaa",
+			"Saumya",
+			"Suraa",
+			"Maayaa",
+			"Manoharaa",
+			"Maadhavi",
+			"Manjuswana",
+			"Ghoraa",
+			"Kumbhini",
+			"Kutilaa",
+			"Prabhaa",
+			"Paraa",
+			"Payaswini",
+			"Maala",
+			"Jagathi",
+			"Jarjharaa",
+			"Dhruva",
+			"TO BE CONTINUED"
+		};
+
+		public static readonly string[] mRulersNadiamsaCKN =
+		{
+			"Vasudha",
+			"Vaishnavi",
+			"Brahmi",
+			"Kalakoota",
+			"Sankari",
+			"Sudhakarasama",
+			"Saumya",
+			"Suraa",
+			"Maaya",
+			"Manoharaa",
+			"Maadhavi",
+			"Manjuswana",
+			"Ghoraa",
+			"Kumbhini",
+			"Kutilaa",
+			"Prabhaa",
+			"Paraa",
+			"Payaswini",
+			"Malaa",
+			"Jagathi",
+			"Jarjhari",
+			"Dhruvaa",
+			"Musalaa",
+			"Mudgala",
+			"Pasaa",
+			"Chambaka",
+			"Daamini",
+			"Mahi",
+			"Kalushaa",
+			"Kamalaa",
+			"Kanthaa",
+			"Kaalaa",
+			"Karikaraa",
+			"Kshamaa",
+			"Durdharaa",
+			"Durbhagaa",
+			"Viswa",
+			"Visirnaa",
+			"Vihwala",
+			"Anilaa",
+			"Bhima",
+			"Sukhaprada",
+			"Snigdha",
+			"Sodaraa",
+			"Surasundari",
+			"Amritaprasini",
+			"Karalaa",
+			"KamadrukkaraVeerini",
+			"Gahwaraa",
+			"Kundini",
+			"Kanthaa",
+			"Vishakhya",
+			"Vishanaasini",
+			"Nirmada",
+			"Seethala",
+			"Nimnaa",
+			"Preeta",
+			"Priyavivardhani",
+			"Manaadha",
+			"Durbhaga",
+			"Chitraa",
+			"Vichitra",
+			"Chirajivini",
+			"Boopa",
+			"Gadaaharaa",
+			"Naalaa",
+			"Gaalavee",
+			"Nirmalaa",
+			"Nadi",
+			"Sudha",
+			"Mritamsuga",
+			"Kaali",
+			"Kaalika",
+			"Kalushankura",
+			"Krailokyamohanakari",
+			"Mahaamaayaa",
+			"Suseethala",
+			"Sukhadaa",
+			"Suprabhaa",
+			"Sobhaa",
+			"Sobhana",
+			"Sivadaa",
+			"Siva",
+			"Balaa",
+			"Jwalaa",
+			"Gadaa",
+			"Gaadaa",
+			"Sootana",
+			"Sumanoharaa",
+			"Somavalli",
+			"Somalatha",
+			"Mangala",
+			"Mudrika",
+			"Sudha",
+			"Melaa",
+			"Apavargaa",
+			"Pasyathaa",
+			"Navaneetha",
+			"Nisachari",
+			"Nivrithi",
+			"Nirgathaa",
+			"Saaraa",
+			"Samagaa",
+			"Samadaa",
+			"Samaa",
+			"Visvambharaa",
+			"Kumari",
+			"Kokila",
+			"Kunjarakrithi",
+			"Indra",
+			"Swaahaa",
+			"Swadha",
+			"Vahni",
+			"Preethaa",
+			"Yakshi",
+			"Achalaprabha",
+			"Saarini",
+			"Madhuraa",
+			"Maitri",
+			"Harini",
+			"Haarini",
+			"Maruthaa",
+			"DHananjaya",
+			"Dhanakari",
+			"Dhanada",
+			"Kaccapa",
+			"Ambuja",
+			"Isaani",
+			"Soolini",
+			"Raudri",
+			"Sivaasivakari",
+			"Kalaa",
+			"Kundaa",
+			"Mukundaa",
+			"Bharata",
+			"Kadali",
+			"Smaraa",
+			"Basitha",
+			"Kodala",
+			"Kokilamsa",
+			"Kaamini",
+			"Kalasodbhava",
+			"Viraprasoo",
+			"Sangaraa",
+			"Sathayagna",
+			"Sataavari",
+			"Sragvi",
+			"Paatalini",
+			"Naagapankaja",
+			"Parameswari"
+		};
+
+		public static readonly string[] avasthas =
+		{
+			"Saisava (child - quarter)",
+			"Kumaara (adolescent - half)",
+			"Yuva (youth - full)",
+			"Vriddha (old - some)",
+			"Mrita (dead - none)"
+		};
+	}
+}
